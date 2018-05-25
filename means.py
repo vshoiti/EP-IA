0a370af883baa0b1c1fc16ec18141526a1d9481b
vshoiti <32967800+vshoiti@users.noreply.github.com>
2018-05-25 20:54:39 -0300

    Pre-processing and WIP k-means updates
    Added input parameters to both scripts.
    Added token blacklist and dense matrix representation to pre.py.

--- means.py
+++ means.py
@@ -1,75 +1,157 @@
 # -*- coding: utf-8 -*-
 
 import sys, random
-import cPickle as pickle
+import cPickle as Pickle
 from math import sqrt
 
-def unpickle(fileName):
-    f = open(fileName, 'r')
-    p = pickle.Unpickler(f)
-    return p.load()
-    
-def listOfAllTokens(corpus):
-    allTokens = []
-    for document in corpus:
-        allTokens = allTokens + list(set(corpus[document]))
-    return allTokens
-    
-def kmeans(corpus, k):
-    allTokens = listOfAllTokens(corpus)
-    centroids = randomInit(allTokens, k)
-    for n in (range(10)):
-        centroidWins = [[] for x in xrange(k)]
-        for text in corpus:
-            closestCentroid = findBMU(centroids, corpus[text], k)
-            centroidWins[closestCentroid].append(corpus[text])
-	print centroidWins
-        newCentroidPositions(centroids, centroidWins)
-    print centroids
+# usage: python means.py [in_filename] [k_int] [epochs_int] [out_filename]
+# example: python means.py reuters_tf_idf.pkl 3 25 out
 
-def newCentroidPositions(centroids, centroidWins):
-    for i in range(len(centroids)):
-        centroid = centroids[i]
-        closestTexts = centroidWins[i]
-        numOfWins = len(closestTexts) if len(closestTexts) > 1 else 1
-        for token in centroid:
-            newTokenValueSum = centroid[token]
-            for text in closestTexts:
+def main():
+    read_file = sys.argv[1]
+    k = int(sys.argv[2])
+    epochs = int(sys.argv[3])
+    #write_file = sys.argv[4]
+    corpus = unpickle(read_file)
+    clusters = batch_kmeans(corpus, k, epochs)
+    print('total silhouette: ', silhouette(clusters))
+
+
+# representa um cluster, com um centroide e uma lista de textos
+class Cluster:
+    def __init__(self, centroid):
+        self.texts = []
+        self.centroid = centroid
+
+    def append(self, text):
+        self.texts.append(text)
+
+    def clear(self):
+        del self.texts[:]
+
+    def re_center_centroid(self):
+        num_of_texts = len(self.texts)
+        if num_of_texts < 1:
+            return
+        for token in self.centroid:
+            new_token_value_sum = float(self.centroid[token])
+            for text in self.texts:
                 try:
-                    newTokenValueSum += text[token]
-                except KeyError: # o texto nao contem aquele token
-                    newTokenValueSum += 0
-            centroid[token] = newTokenValueSum / numOfWins
+                    new_token_value_sum += float(text[token])
+                except KeyError:  # o texto nao contem aquele token
+                    new_token_value_sum += 0
+            self.centroid[token] = float(new_token_value_sum / num_of_texts)
 
-def findBMU(centroids, text, k):
-    minDistance = -1
-    closestCentroid = -1
-    for i in range(k):
-        currentDist = dist(centroids[i], text)
-        if currentDist < minDistance or minDistance == -1:
-            minDistance = currentDist
-            closestCentroid = i
-    return closestCentroid
-            
-def dist(centroid, text):
+    def avg_dist_to_cluster(self, text):
+        avg_dist = 0
+        for txt in self.texts:
+            avg_dist += dist(text, txt)
+        return avg_dist / float((len(self.texts)))
+
+
+def unpickle(file_name):
+    f = open(file_name, 'r')
+    p = Pickle.Unpickler(f)
+    return p.load()
+
+
+# retorna todos os tokens unicos em um corpus
+def list_of_all_tokens(corpus):
+    all_tokens = []
+    for document in corpus:  # itera sobre todos os documentos
+        all_tokens = list(set(all_tokens + corpus[document].keys()))  # adiciona todos os tokens de documento a lista
+    return list(set(all_tokens))  # retorna todos os tokens sem repeticao
+
+
+def batch_kmeans(corpus, k, epochs):
+    clusters = generate_clusters(corpus, k)
+    for n in (range(epochs)):  # loop de epocas
+        clear_clusters(clusters)
+        for text in corpus:
+            bmu = find_bmu(clusters, corpus[text])
+            bmu.append(corpus[text])
+        re_center_centroids(clusters)
+        #print("epoch ", n)
+        #for cluster in clusters:
+        #    print(len(cluster.texts))
+        #    if len(cluster.texts) < 1: sys.exit(-1)
+    return clusters
+
+
+def find_bmu(clusters, text):
+    min_distance = sys.maxsize
+    closest_centroid = None
+    for cluster in clusters:
+        current_dist = dist(cluster.centroid, text)
+        if current_dist < min_distance:
+            min_distance = current_dist
+            closest_centroid = cluster
+    return closest_centroid
+
+
+def dist(text1, text2):
     sum = 0
-    for token in centroid:
+    for token in text1:
         try:
-            sum += (centroid[token] - text[token]) ** 2
-        except KeyError: # o texto nao contem aquele token
-            sum += (centroid[token] - 0) ** 2
+            sum += (float(text1[token]) - float(text2[token])) ** 2
+        except KeyError:  # o texto2 nao contem aquele token
+            sum += float(text1[token] - 0) ** 2
     return sqrt(sum)
 
-def randomInit(allTokens, k):
+
+def generate_clusters(corpus, k):
+    all_tokens = list_of_all_tokens(corpus)
+    centroids = random_init(all_tokens, k)
+    clusters = []
+    for centroid in centroids:
+        clusters.append(Cluster(centroid))
+    return clusters
+
+
+def random_init(all_tokens, k):
     centroids = []
+    random.seed()
     for i in range(k):
         centroid = {}
-        for token in allTokens:
-            centroid[token] = random.uniform(0,1)
+        for token in all_tokens:
+            centroid[token] = random.uniform(0, 1)
         centroids.append(centroid)
     return centroids
 
-readFile = sys.argv[1]
-writeFile = sys.argv[2]
-corpus = unpickle(readFile)
-corpus2 = kmeans(corpus,3)
+
+def re_center_centroids(clusters):
+    for cluster in clusters:
+        cluster.re_center_centroid()
+
+
+def clear_clusters(clusters):
+    for cluster in clusters:
+        cluster.clear()
+
+def silhouette(clusters):
+    silhouette_sum = 0
+    for cluster in clusters:
+        if len(cluster.texts) < 1:
+            print('cluster ', cluster, ' is empty')
+            continue
+        other_clusters = clusters[:]  # shallow copy of clusters array
+        other_clusters.remove(cluster)
+        closest_cluster = find_bmu(other_clusters, cluster.centroid)
+        while len(closest_cluster.texts) < 1:
+            other_clusters.remove(closest_cluster)
+            closest_cluster = find_bmu(other_clusters, cluster.centroid)
+        cluster_silhouette_sum = 0
+        for text in cluster.texts:
+            cluster_avg_dist = cluster.avg_dist_to_cluster(text)  # a(text)
+            closest_cluster_avg_dist = closest_cluster.avg_dist_to_cluster(text)  # b(text)
+            cluster_silhouette_sum += ((closest_cluster_avg_dist - cluster_avg_dist)
+                                       / max(closest_cluster_avg_dist, cluster_avg_dist))
+
+        cluster_silhouette = cluster_silhouette_sum / len(cluster.texts)
+        print('silhouette for ', cluster, ': ', cluster_silhouette)
+        silhouette_sum += cluster_silhouette
+    return silhouette_sum / len(clusters)
+
+
+if __name__ == '__main__':
+    main()
--- pre.py
+++ pre.py
@@ -1,39 +1,63 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 
 import os
 import re
 import sys
-import cPickle as pickle
+import pickle
 import nltk
+import operator
 from nltk import SnowballStemmer
 from nltk.corpus import stopwords
 from math import log10
 
+# usage: python pre.py [in_filename] [representation_char] [stemmer_binary] [cut_binary] [out_filename]
+# example: python pre.py reuters.txt b 1 0 reuters_bin_stem
+# b: binary
+# t: tf
+# i: tf-idf
+
 def main():
     # estagio de preparacao
     sourcepath = sys.argv[1]
-    targetpath = sys.argv[2]
+    representation = sys.argv[2]
+    stemmer = True if int(sys.argv[3]) == 1 else False
+    cut = sys.argv[4]
+    targetpath = sys.argv[5]
     corpus1 = loadCorpus(sourcepath)
+    #print('corpus1 len(): ', len(corpus1))
     # estagio de pre-processamemento
     param_foldCase = True
     param_language = 'english'
     param_listOfStopWords = stopwords.words(param_language)
-    param_stemmer = SnowballStemmer(param_language)
+    param_stemmer = SnowballStemmer(param_language) if stemmer else None
     params = (param_foldCase, param_listOfStopWords, param_stemmer)
     corpus2 = processCorpus(corpus1, params)
+    freq_list = token_frequency_list(corpus2) # lista que contem todos os tokens e sua frequencia
+    #print len(freq_list)
+    num_terms_to_remove = len(freq_list)/200 * int(cut) # removeremos tokens acima do 99.5 percentil
+    #print num_terms_to_remove
+    blacklist = black_list(freq_list, num_terms_to_remove)
+    #blacklist.append('aaa')
+    #print len(blacklist)
     # estagio de representacao
-    # remova o '#' de uma das linhas abaixo 
-    #corpus3 = representCorpus_binary(corpus2)
-    #corpus3 = representCorpus_tf(corpus2)
-    #corpus3 = representCorpus_tf_idf(corpus2)
+    if representation == 'b':
+        corpus3 = representCorpus_binary(corpus2, blacklist)
+    if representation == 't':
+        corpus3 = representCorpus_tf(corpus2, blacklist)
+    if representation == 'i':
+        corpus3 = representCorpus_tf_idf(corpus2, blacklist)
     serialise(corpus3, targetpath)
+    corpus4 = represent_matrix(corpus3)
+    serialise(corpus4, targetpath+"_som")
+    #print corpus4
+
 
 def loadCorpus(sourcepath):
     corpus = {}
     with open(sourcepath, 'r') as f:
         for line in f:
-        	if line.strip(): # se a line nao for vazia, line.strip() == true
-	            corpus[line] = line
+            if line.strip(): # se a line nao for vazia, line.strip() == true
+                corpus[line] = line
     return corpus
 
 def processCorpus(corpus, params):
@@ -44,7 +68,9 @@ def processCorpus(corpus, params):
         text = foldCase(text, param_foldCase)
         listOfTokens = tokenize(text)
         listOfTokens = removeStopWords(listOfTokens, param_listOfStopWords)
-        #listOfTokens = applyStemming(listOfTokens, param_stemmer)
+        if (param_stemmer):
+            listOfTokens = applyStemming(listOfTokens, param_stemmer)
+        
         newCorpus[text] = listOfTokens
     return newCorpus
 
@@ -63,7 +89,7 @@ def removeStopWords(listOfTokens, listOfStopWords):
 def applyStemming(listOfTokens, stemmer):
     return [stemmer.stem(token) for token in listOfTokens]
 
-def representCorpus_tf_idf(corpus):
+def representCorpus_tf_idf(corpus, blacklist):
     # cria uma lista com todos os tokens distintos que ocorrem em cada documento.
     allTokens = []
     for document in corpus:
@@ -85,38 +111,97 @@ def representCorpus_tf_idf(corpus):
     # computa a matriz termo-documento (newCorpus)
     for document in corpus:
         # computa um dicionario com os tf scores de cada termo que ocorre no documento
-        dictOfTfScoredTokens = tf(corpus[document])
+        dictOfTfScoredTokens = tf(corpus[document], blacklist)
         # computa um dicionario com o tf-idf score de cada par termo-documento
         corpus[document] = ({token: dictOfTfScoredTokens[token] * idfDict[token] for token in dictOfTfScoredTokens})
     return corpus
 
-def representCorpus_binary(corpus):
-	newCorpus = {}
-	for document in corpus:
-		newCorpus[document] = binary(corpus[document])
-	return newCorpus
+def representCorpus_binary(corpus, blacklist):
+    newCorpus = {}
+    for document in corpus:
+        newCorpus[document] = binary(corpus[document], blacklist)
+    return newCorpus
 
-def representCorpus_tf(corpus):
-	newCorpus = {}
-	for document in corpus:
-		newCorpus[document] = tf(corpus[document])
-	return newCorpus
+def representCorpus_tf(corpus, blacklist    ):
+    newCorpus = {}
+    for document in corpus:
+        newCorpus[document] = tf(corpus[document], blacklist)
+    return newCorpus
 
-def tf(listOfTokens):
+def tf(listOfTokens, blacklist):
     # cria um dicionario associando cada token com o numero de vezes
     # em que ele ocorre no documento (cujo conteudo eh listOfTokens)
     types = {}
     for token in listOfTokens:
-        if(token in types.keys()): types[token] += 1
-        else: types[token] = 1
+        try:
+            types[token] += 1
+        except KeyError:
+            types[token] = 1
+
+    types = removeTokens(types, blacklist)
     return types
     
-def binary(listOfTokens):
+def binary(listOfTokens, blacklist):
     types = {}
     for token in listOfTokens:
         types[token] = 1
+
+    types = removeTokens(types, blacklist)
     return types
-	
+
+def removeTokens(tokenDict, blacklist):
+    for token in blacklist:
+        try:
+            del tokenDict[token]
+        except KeyError:
+            continue
+    return tokenDict
+
+def token_frequency_list(corpus):
+    frequency_list = {}
+    for text in corpus:
+        frequency_list = add_dicts(tf(corpus[text], []), frequency_list)
+    return frequency_list
+
+def black_list(freq_list, num_terms_to_remove):
+    blacklist = []
+    for word in freq_list.keys(): # para cada palavra que aparece 1 vez
+        if (freq_list[word] == 1): blacklist.append(word)
+
+    # ordena decresc. a lista de freq. em uma lista de palavras 
+    sorted_key_list = sorted(freq_list.items(), key=operator.itemgetter(1), reverse=True) # https://stackoverflow.com/a/613218
+    for i in range(num_terms_to_remove): # adiciona as 'num_terms'-primeiras palavras a blacklist
+        #print sorted_key_list[i][0]
+        blacklist.append(sorted_key_list[i][0])
+    return blacklist
+
+def add_dicts(partial_dict, total_dict):
+    for token in partial_dict:
+        try:
+            total_dict[token] += 1
+        except KeyError:
+            total_dict[token] = 1
+    return total_dict
+
+def represent_matrix(corpus):
+    allTokens = []
+    for document in corpus:
+        allTokens = list(set(allTokens + corpus[document].keys()))    
+
+    new_corpus = []
+    new_corpus.append(allTokens)
+    for text in corpus:
+        document = []
+        for token in allTokens:
+            try:
+                document.append(corpus[text][token])
+            except KeyError:
+                document.append(0)
+        new_corpus.append(document)
+    
+    return new_corpus
+            
+
 def serialise(obj, name):
     f = open(name + '.pkl', 'wb')
     p = pickle.Pickler(f)
@@ -125,5 +210,5 @@ def serialise(obj, name):
     f.close()
     p.clear_memo()
 
-nltk.download('stopwords')
+#nltk.download('stopwords')
 main()
