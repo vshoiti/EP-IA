fc91683fdd9cd372cf44ae7101721e59ee4b14d0
vshoiti <32967800+vshoiti@users.noreply.github.com>
2018-05-27 12:50:58 -0300

    Added cosine similarity distance and serialisation

--- means.py
+++ means.py
@@ -3,52 +3,68 @@
 import sys, random
 import cPickle as Pickle
 from math import sqrt
+import time
 
-# usage: python means.py [in_filename] [k_int] [epochs_int] [out_filename]
-# example: python means.py reuters_tf_idf.pkl 3 25 out
+# usage: python means.py [in_filename] [k_int] [epochs_int] [dist_char] [out_filename]
+# example: python means.py reuters_tf_idf.pkl 3 25 s out
+# d: euclidean distance
+# s: cosine similarity
+
 
 def main():
+    # todo: comment
     read_file = sys.argv[1]
     k = int(sys.argv[2])
     epochs = int(sys.argv[3])
-    #write_file = sys.argv[4]
+    write_file = sys.argv[5]
     corpus = unpickle(read_file)
+    if (sys.argv[4] == 's'):
+        dist = dist_cos_simil
+        magnitude = set_magnitude(corpus)
     clusters = batch_kmeans(corpus, k, epochs)
     print('total silhouette: ', silhouette(clusters))
+    serialise(clusters, write_file)
 
 
 # representa um cluster, com um centroide e uma lista de textos
 class Cluster:
+    # inicializa o Cluster com um centroide que o representa e uma lista de textos
     def __init__(self, centroid):
         self.texts = []
         self.centroid = centroid
 
+    # adiciona um texto a lista de textos que pertencem ao cluster
     def append(self, text):
         self.texts.append(text)
 
+    # limpa a lista de textos que pertencem ao cluster
     def clear(self):
         del self.texts[:]
 
+    # reposiciona o centroid no espaco vetorial no centro de massa de seus textos
     def re_center_centroid(self):
         num_of_texts = len(self.texts)
-        if num_of_texts < 1:
-            return
-        for token in self.centroid:
-            new_token_value_sum = float(self.centroid[token])
-            for text in self.texts:
+        if num_of_texts < 1:  # caso o cluster nao possui textos
+            return  # nao faca nada
+        for token in self.centroid:  # itera por cada token no centroide
+            new_token_value_sum = 0  # guarda a soma dos valores que o token assume nos textos
+            for text in self.texts: # itera pelos textos do cluster
                 try:
-                    new_token_value_sum += float(text[token])
+                    new_token_value_sum += float(text[token]) # adiciona a sum o valor do token no texto
                 except KeyError:  # o texto nao contem aquele token
                     new_token_value_sum += 0
-            self.centroid[token] = float(new_token_value_sum / num_of_texts)
+            self.centroid[token] = float(new_token_value_sum / num_of_texts)  # setta para o valor medio no cluster
 
+    # calcula a distancia media de um texto para todos os textos em um cluster
+    # utilizado no silhouette index
     def avg_dist_to_cluster(self, text):
-        avg_dist = 0
-        for txt in self.texts:
-            avg_dist += dist(text, txt)
-        return avg_dist / float((len(self.texts)))
+        dist_sum = 0 # armazena a soma de distancias
+        for cluster_text in self.texts: # itera pelos textos do cluster
+            dist_sum += dist(text, cluster_text) # soma a distancia
+        return dist_sum / float((len(self.texts))) # retorna a distancia media
 
 
+# todo: comment
 def unpickle(file_name):
     f = open(file_name, 'r')
     p = Pickle.Unpickler(f)
@@ -57,48 +73,55 @@ def unpickle(file_name):
 
 # retorna todos os tokens unicos em um corpus
 def list_of_all_tokens(corpus):
-    all_tokens = []
-    for document in corpus:  # itera sobre todos os documentos
-        all_tokens = list(set(all_tokens + corpus[document].keys()))  # adiciona todos os tokens de documento a lista
-    return list(set(all_tokens))  # retorna todos os tokens sem repeticao
+    all_tokens = [] # lista que armazena todos os tokens
+    for document in corpus.values():  # itera sobre todos os documentos
+        all_tokens = list(set(all_tokens + document.keys()))  # adiciona todos os tokens de documento a lista
+    return list(set(all_tokens))  # converte a lista para set para eliminar repeticoes e converte para lista de novo
 
 
+# todo: commenttext_dist_simil
 def batch_kmeans(corpus, k, epochs):
     clusters = generate_clusters(corpus, k)
     for n in (range(epochs)):  # loop de epocas
         clear_clusters(clusters)
-        for text in corpus:
-            bmu = find_bmu(clusters, corpus[text])
-            bmu.append(corpus[text])
+        for text in corpus.values():
+            bmu = find_bmu(clusters, text)
+            bmu.append(text)
         re_center_centroids(clusters)
-        #print("epoch ", n)
-        #for cluster in clusters:
+        print("epoch ", n)
+        for cluster in clusters:
         #    print(len(cluster.texts))
-        #    if len(cluster.texts) < 1: sys.exit(-1)
+            if len(cluster.texts) < 1: sys.exit(-1)
     return clusters
 
 
+# retorna o cluster cujo centroide esta mais proximo de text
 def find_bmu(clusters, text):
-    min_distance = sys.maxsize
-    closest_centroid = None
-    for cluster in clusters:
-        current_dist = dist(cluster.centroid, text)
-        if current_dist < min_distance:
+    min_distance = sys.maxsize # inicializa a distancia como o maior int possivel
+    closest_cluster = None # inicializa o centroid mais prox como nulo
+    for cluster in clusters: # itera pelos clusters
+        current_dist = dist(cluster.centroid, text) # calcula a distancia entre o centroide e o texto
+        if current_dist < min_distance: # atualiza os minimos se a distancia encontrada for a menor ate agora
             min_distance = current_dist
-            closest_centroid = cluster
-    return closest_centroid
+            closest_cluster = cluster
+    return closest_cluster # retorna c cluster vencedor
 
 
-def dist(text1, text2):
-    sum = 0
-    for token in text1:
-        try:
-            sum += (float(text1[token]) - float(text2[token])) ** 2
-        except KeyError:  # o texto2 nao contem aquele token
-            sum += float(text1[token] - 0) ** 2
-    return sqrt(sum)
+# todo: comment
+# retorna a distancia euclideana entre um texto e outro texto
+# https://stackoverflow.com/a/38370159
+def dist_euclid(text1, text2):
+    return sqrt(sum((text1.get(token, 0) - text2.get(token, 0)) ** 2 for token in set(text1) | set(text2)))
 
 
+# todo: comment
+# https://stackoverflow.com/a/38370159
+def dist_cos_simil(text1, text2):
+    sum = sum(text1.get(token, 0)*text2.get(token, 0) for token in set(text1) | set(text2))
+    return 1 - (sum / (magnitude[text1] * magnitude[text2]))
+
+
+# todo: comment
 def generate_clusters(corpus, k):
     all_tokens = list_of_all_tokens(corpus)
     centroids = random_init(all_tokens, k)
@@ -108,6 +131,7 @@ def generate_clusters(corpus, k):
     return clusters
 
 
+# todo: comment
 def random_init(all_tokens, k):
     centroids = []
     random.seed()
@@ -119,15 +143,19 @@ def random_init(all_tokens, k):
     return centroids
 
 
+# todo: comment
 def re_center_centroids(clusters):
     for cluster in clusters:
         cluster.re_center_centroid()
 
 
+# todo: comment
 def clear_clusters(clusters):
     for cluster in clusters:
         cluster.clear()
 
+
+# todo: comment
 def silhouette(clusters):
     silhouette_sum = 0
     for cluster in clusters:
@@ -148,10 +176,30 @@ def silhouette(clusters):
                                        / max(closest_cluster_avg_dist, cluster_avg_dist))
 
         cluster_silhouette = cluster_silhouette_sum / len(cluster.texts)
-        print('silhouette for ', cluster, ': ', cluster_silhouette)
+        print('silhouette for ', cluster, ', ', len(cluster.texts), cluster_silhouette)
         silhouette_sum += cluster_silhouette
     return silhouette_sum / len(clusters)
 
 
+# todo: comment
+def set_magnitude(corpus):
+    for document in corpus:
+        magnitude[document] = sqrt(sum([corpus[document][token] ** 2 for token in corpus[document]]))
+    return set_magnitude
+
+
+def serialise(obj, name):
+    f = open(name + '.pkl', 'wb')
+    p = Pickle.Pickler(f)
+    p.fast = True
+    p.dump(obj)
+    f.close()
+    p.clear_memo()
+
 if __name__ == '__main__':
+    start = time.clock()
+    dist = dist_euclid # setta a distancia euclideana como padrao
+    magnitude = {} # declara um dicionario que armazenara a magnitude dos vetores para a similaridade de cossenos
     main()
+    end = time.clock()
+    print(end-start)
